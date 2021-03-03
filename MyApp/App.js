0aa5466375273a92a2b4aac463c07e9d1e6e1812
toshi-toma <toshitoma1217@gmail.com>
2021-03-03 17:08:58 +0900

    Init Project

---
+++ MyApp/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.text}>
+      <Text>Hello, world! React Native</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
